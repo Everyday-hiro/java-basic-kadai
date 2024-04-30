2936f48a8d6c3a1f2d56e006a56358d1cc702df3
Everyday-hiro <hiromu00815@gmail.com>
2024-04-30 20:27:06 +0900

    質問

---
+++ kadai_017/Kato_Chapter17.java
@@ -0,0 +1,14 @@
+package kadai_017;
+
+abstract public class Kato_Chapter17 {
+
+	final String familyName = "加藤";
+	String givenName = "";
+	final String address = "住所は東京都中野区〇×です";
+	
+	public void commonIntroduce() {
+		
+	}
+	
+	abstract public void eachIntroduce();
+}
